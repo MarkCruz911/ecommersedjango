a8614028ed427b82bed7d5e12f4befc7f21a5f6d
MarkCruz911 <85602094+MarkCruz911@users.noreply.github.com>
2025-04-12 12:44:25 -0400

    register verified

--- accounts/views.py
+++ accounts/views.py
@@ -26,34 +26,35 @@ def register(request):
             phone_number = form.cleaned_data['phone_number']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-            username = email.split("@")[0]  # FALTA PHONE NUMBEEEEEEEEEEEEEEEER
+            username = email.split("@")[0]  # Username basado en el email
+
+            # Crear el usuario
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
 
-            profile = UserProfile()
-            profile.user_id = user.id
-            profile.profile_picture = 'default/default-user.png'
-            profile.save()
+            # Verificar si el perfil de usuario ya existe
+            if not hasattr(user, 'userprofile'):
+                profile = UserProfile()
+                profile.user = user  # Asignar el perfil al usuario
+                profile.profile_picture = 'default/default-user.png'
+                profile.save()
 
-
+            # Enviar email de verificación
             current_site = get_current_site(request)
             mail_subject = 'Activa tu cuenta en ByteShop para continuar'
             body = render_to_string('accounts/account_verification_email.html', {
                 'user': user,
                 'domain': current_site,
-                'uid': urlsafe_base64_encode( force_bytes(user.pk)),
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
                 'token': default_token_generator.make_token(user),
             })
             to_email = email
             send_email = EmailMessage(mail_subject, body, to=[to_email])
             send_email.send()
 
-
-            # messages.success(request, 'Te has registrado exitosamente')
+            # Redirigir al login para verificar la cuenta
             return redirect('/accounts/login/?command=verification&email='+email)
-
-
 
     context = {
         'form': form
